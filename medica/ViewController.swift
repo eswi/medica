a6e630f1135e3aa164f6c959d985cff307363b86
Euiseok Wi <me@teri.kr>
2020-01-31 20:00:34 +0900

    Initial Commit

---
+++ medica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  medica
+//
+//  Created by 위의석 on 2020/01/31.
+//  Copyright © 2020 Wi's Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
